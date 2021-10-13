036fa176c98f23533df37e45256e4f2b5ca13acc
katesavinova <savinova.katerina@inbox.ru>
2021-10-13 18:47:37 +0400

    popUp

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello!')
+function click_menu_basket(){
+  document.getElementById("popup-block").style.display = 'block';
+}
+function click_close_menu_basket(){
+  document.getElementById("popup-block").style.display = 'none';
+}
